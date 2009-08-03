1051266af1236ec64e19f5147cfec03c65c12e52
Frederik Ramm <frederik@remote.org>
2009-08-03 09:22:41 +0000

    allow overriding of default comment when reverting a changeset

--- revert.pl
+++ revert.pl
@@ -11,22 +11,35 @@ use Changeset;
 if (scalar(@ARGV) < 1 || scalar(@ARGV) > 2)
 {
     print <<EOF;
-usage: $0 <changeset_to_undo> [ <current_changeset> ]
+usage: $0 <changeset_to_undo> [ <current_changeset> | <comment>]
 
 where 
   changeset_to_undo : is the id of the changeset you want to revert
   current_changeset : is the id of a currently open changeset under
      which this action will run.
-     If unset, a new changeset will be created.
+     If this is given, the changeset will not be closed in order
+     to allow you to run multiple operations on the same changeset.
+     If unset, a new changeset will be created and closed afterwards.
+  comment : the comment to use when closing the changeset (must not
+     be numeric lest it would be interpreted as a changeset id).
 EOF
     exit;
 }
 
-my ($undo_cs, $current_cs) = @ARGV;
+my ($undo_cs, $current_cs_or_comment) = @ARGV;
 my $do_close = 0;
 
-if (!defined($current_cs))
+my $current_cs;
+my $comment; 
+
+# what have we got, changeset or comment?
+if ($current_cs_or_comment =~ /^\d+$/)
 {
+    $current_cs = $current_cs_or_comment;
+}
+else
+{
+    $comment = $current_cs_or_comment;
     $current_cs = Changeset::create();
     $do_close = 1;
 }
@@ -37,7 +50,8 @@ if (defined($current_cs))
     {
         if ($do_close)
         {
-            Changeset::close($current_cs, "reverted changeset $undo_cs");
+            $comment = "reverted changeset $undo_cs" if ($comment eq "");
+            Changeset::close($current_cs, $comment);
         }
     }
 }
