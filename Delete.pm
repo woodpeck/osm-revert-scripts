00fa88334e2eb96ffeacac43765de6bb13752bf9
Frederik Ramm <frederik@remote.org>
2009-10-02 07:15:41 +0000

    add capability to remove an object that is to be deleted from a referring object (i.e. you delete a node and it is automatically removed from a way or relation that uses this node, instead of the delete failing with "precondition failed")

--- Delete.pm
+++ Delete.pm
@@ -31,16 +31,57 @@ our $globalListOfDeletedStuff = {};
 sub delete
 {
     my ($what, $id, $changeset) = @_;
+    # this will try to remove not only the object but all its members
+    # e.g. remove a way plus nodes
     my $recurse = 0;
+    # this will try to modify any object that contains the object-to-be-deleted
+    # by removing the object-to-be-deleted from it
+    my $remove_references = 0;
 
     my $xml = determine_delete_action($what, $id, $changeset, $recurse, 0);
     return undef unless defined ($xml);
 
-    my $resp = OsmApi::post("changeset/$changeset/upload", "<osmChange version='0.6'>\n<delete>\n$xml</delete></osmChange>");
-    if (!$resp->is_success)
+    my $modify = "";
+    my $loop = 1;
+    while ($loop)
     {
-        print STDERR "$what $id cannot be deleted: ".$resp->status_line."\n";
-        return undef;
+        $loop = 0;
+        my $osc = <<EOF;
+<osmChange version='0.6'>
+<modify>
+$modify
+</modify>
+<delete>
+$xml
+</delete>
+</osmChange>
+EOF
+        my $resp = OsmApi::post("changeset/$changeset/upload", "<osmChange version='0.6'>\n<modify>\n$modify\n</modify>\n<delete>\n$xml</delete></osmChange>");
+        if (!$resp->is_success)
+        {
+            my $c = $resp->content();
+            if ($remove_references && ($c =~ /still used by (\S+) (\d+)/))
+            {
+                print STDERR "$what $id still used by $1 $2; removing it from there\n";
+                my $obj = OsmApi::get("$1/$2");
+                foreach (split(/\n/, $obj->content()))
+                { 
+                    next if (/<\?xml/);
+                    next if (/<osm/);
+                    next if (/<\/osm/);
+                    next if (/<nd ref="$id"/) && ($what eq "node");
+                    next if (/<member type="$what" ref="$id"/);
+                    s/changeset="\d+"/changeset="$changeset"/;
+                    $modify .= $_;
+                }
+                $loop=1;
+            }
+            else
+            {
+                print STDERR "$what $id cannot be deleted: ".$resp->status_line."\n";
+                return undef;
+            }
+        }
     }
     return 1;
 }
@@ -87,7 +128,7 @@ sub determine_delete_action
             $user=$1;
             $copy = 1;
             $out = $_;
-	    $out =~ s/">/"\/>/g;
+            $out =~ s/">/"\/>/g;
             $members = [];
         } 
         elsif ($copy) 
@@ -107,21 +148,21 @@ sub determine_delete_action
     print STDERR " "x$indent;
     print STDERR "$what $id last modified by $user (version $version) - deleting\n",
     $out =~ s/changeset="\d+"/changeset="$changeset"/;
-        if ($recursive && scalar(@$members))
+    if ($recursive && scalar(@$members))
+    {
+        print STDERR " "x$indent;
+        print STDERR "recursively deleting members of $what $id\n";
+        foreach (@$members)
         {
-            print STDERR " "x$indent;
-            print STDERR "recursively deleting members of $what $id\n";
-            foreach (@$members)
+            if (!defined($globalListOfDeletedStuff->{$_->{type}.$_->{id}}))
             {
-                if (!defined($globalListOfDeletedStuff->{$_->{type}.$_->{id}}))
-                {
-                    my $ua = determine_delete_action($_->{type}, $_->{id}, $changeset, 1, $indent + 2);
-                    $out = $ua . $out if defined($ua);
-                    $globalListOfDeletedStuff->{$_->{type}.$_->{id}} = 1;
-                }
+                my $ua = determine_delete_action($_->{type}, $_->{id}, $changeset, 1, $indent + 2);
+                $out = $ua . $out if defined($ua);
+                $globalListOfDeletedStuff->{$_->{type}.$_->{id}} = 1;
             }
         }
-        return $out;
+    }
+    return $out;
 }
 
 1;
