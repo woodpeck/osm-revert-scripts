02ca1a802a3fccb73d64f347b8806db10df82c55
Anton Khorev <tony29@yandex.ru>
2021-05-16 12:22:01 +0300

    produce two element lists on fail

--- BatchRedaction.pm
+++ BatchRedaction.pm
@@ -70,28 +70,86 @@ sub view
 sub apply
 {
     my ($filename, $rid) = @_;
-    my $state = 1;
-    open(FH, '<', $filename) or return undef;
 
+    my $done_exists = -e "$filename.done";
+    my $left_exists = -e "$filename.left";
+    if ($done_exists || $left_exists)
+    {
+        my $file_word = (($done_exists && $left_exists) ? "files" : "file");
+        print "Cannot continue because $file_word produced by an interrupted batch redaction exist:\n";
+        if ($done_exists)
+        {
+            print "    $filename.done with elements that have redaction calls completed\n";
+            print "        - you may put this file away where you store completed redactions\n";
+        }
+        if ($left_exists)
+        {
+            print "    $filename.left with elements that have redaction calls failed or not attempted\n";
+            print "        - you may replace the original file $filename with this one and run again\n";
+        }
+        print "Please review and rename/remove the $file_word before continuing.\n";
+        return undef;
+    }
+
+    my $state = 1;
+    my (@done_elements, @left_elements);
+    open(FH, '<', $filename) or return undef;
     while(<FH>)
     {
         chomp;
-        print "redacting $_\n";
+
+        if (!$state)
+        {
+            push @left_elements, $_;
+            next;
+        }
+
+        print "redacting $_";
         my $path = "$_/redact";
         $path .= "?redaction=$rid" if defined $rid;
         my $resp = OsmApi::post($path);
 
-        if (!$resp->is_success)
+        if ($resp->is_success)
         {
+            print " - done\n";
+            push @done_elements, $_;
+        }
+        else
+        {
+            print " - failed\n";
+            push @left_elements, $_;
             my $m = $resp->content;
             $m =~ s/\s+/ /g;
             print STDERR "cannot redact $_: ".$resp->status_line.": $m\n";
             $state = undef;
-            last;
         }
     }
-
     close(FH);
+
+    sub write_elements_file
+    {
+        my ($output_filename, @output_elements) = @_;
+        my $output_fh;
+
+        return if (!@output_elements);
+        if (!open $output_fh, '>', $output_filename)
+        {
+            print STDERR "cannot open interrupted batch redaction file $output_filename";
+            return
+        }
+        foreach (@output_elements)
+        {
+            print $output_fh "$_\n";
+        }
+        close $output_fh;
+    }
+
+    if (!$state)
+    {
+        write_elements_file "$filename.done", @done_elements;
+        write_elements_file "$filename.left", @left_elements;
+    }
+
     return $state;
 }
 
