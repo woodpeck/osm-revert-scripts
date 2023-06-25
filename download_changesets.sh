35f318d903bf627c18e844cc3d85b53c24b9132e
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2023-06-26 00:52:31 +0100

    Made download_changesets.sh work again, albeit without the check for "multiple changesets in the same second", which isn't a common problem.

--- download_changesets.sh
+++ download_changesets.sh
@@ -6,7 +6,7 @@
 # therefore loop required
 
 USER=$1
-SINCE=2013-11-01T00:00:00
+SINCE=2023-06-25T20:00:00
 
 # no user servicable parts below. run this in empty directory 
 # and you'll end up with tons of files called c1234.osc (one
@@ -17,14 +17,30 @@ export T
 EX=0
 export EX
 
+# There are two loops here.  We exit the outer one if we try 
+# to obtain some changesets and fail to do so
+# ("EX=1" means "exit the outer loop").
+# The inner loop is just determined by reading the list.
+
 while [ $EX = 0 ]
 do
-    wget -Olist "https://api.openstreetmap.org/api/0.6/changesets?display_name=$USER&time=$SINCE,$T" 
-    T=`grep "<changeset" list | tail -1 | cut -d\" -f4`
-    T=`date +"%Y-%m-%dT%H:%M:%SZ" -u -d "$T + 1 second"`
+    wget -Olist.$$ "https://api.openstreetmap.org/api/0.6/changesets?display_name=$USER&time=$SINCE,$T" 
+    T=`grep "<changeset" list.$$ | tail -1 | cut -d\" -f4`
 
-    EX=1
-    cat list | grep "<changeset" | cut -d\" -f2 | while read id
+# A previous version attempted to check for "multiple changesets in the same second" by doing
+#    T=`date +"%Y-%m-%dT%H:%M:%SZ" -u -d "$T + 1 second"`
+# That didn't work but went unnoticed because the outer loop exit logic was always true previously 
+# With the required "if" just below, it was always false and looped forever.
+# (the last changeset we had just read was always still there).
+
+    if grep -q '<changeset' list.$$
+    then
+	EX=0
+    else
+	EX=1
+    fi
+    
+    cat list.$$ | grep '<changeset' | cut -d\" -f2 | while read id
     do
         if [ -f c$id.osc ]
         then
@@ -36,4 +52,4 @@ do
     done
 done
 
-rm -f list
+rm -f list.$$
