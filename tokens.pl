3a52bf587d7a319c84ca3684dc014a45d1f0c0e9
Anton Khorev <tony29@yandex.ru>
2023-07-27 15:33:33 +0300

    print user id/name for stored tokens

--- tokens.pl
+++ tokens.pl
@@ -7,13 +7,18 @@ use OsmApi;
 
 if (($ARGV[0] eq "request") && (scalar(@ARGV) == 1))
 {
-    request_tokens()
+    request_tokens();
+}
+elsif (($ARGV[0] eq "check") && (scalar(@ARGV) == 1))
+{
+    check_tokens();
 }
 else
 {
     print <<EOF;
 Usage: 
   $0 request    request oauth2 tokens
+  $0 check      check user details of stored tokens
 EOF
     exit;
 }
@@ -39,4 +44,50 @@ sub request_tokens
         print "\n=== Requesting the secondary token. ===\n\nLogin with your bot/mechanical edit account.\nAltenatively, if you want to use only one account, interrupt the script.\n";
         OsmApi::request_oauth2_token("oauth2_token_secondary");
     }
+}
+
+sub check_tokens
+{
+    if (OsmApi::check_oauth2_token("oauth2_token"))
+    {
+        print "Primary token details:\n";
+        print_token_details(1);
+    }
+    else
+    {
+        print "No primary token stored.\n\n";
+    }
+
+    if (OsmApi::check_oauth2_token("oauth2_token_secondary"))
+    {
+        print "Secondary token details:\n";
+        print_token_details();
+    }
+    else
+    {
+        print "No secondary token stored.\n\n";
+    }
+}
+
+sub print_token_details
+{
+    my ($primary) = @_;
+    my $resp = OsmApi::get("user/details", undef, $primary);
+    if (!$resp->is_success)
+    {
+        print "- failed to get user details\n";
+    }
+    else
+    {
+        open my $fh, '<', \$resp->content;
+        while (<$fh>)
+        {
+            if (/<user/)
+            {
+                print "- display name: $1\n" if (/display_name="([^"]+)"/);
+                print "- id: $1\n" if (/id="([^"]+)"/);
+            }
+        }
+    }
+    print "\n";
 }
