718142b83694d0b067489df9111384c1551f8ac4
Anton Khorev <tony29@yandex.ru>
2023-08-27 20:58:51 +0300

    draw changeset ids in graph nodes

--- graph.js
+++ graph.js
@@ -2,4 +2,27 @@ const Graph = ForceGraph()
 (document.getElementById('graph'))
     .graphData(gData)
     .linkWidth(({weight}) => 32*Math.log(weight)/Math.log(10000))
-    .linkDirectionalArrowLength(6);
+    .linkDirectionalArrowLength(6)
+    // https://github.com/vasturiano/force-graph/blob/master/example/text-nodes/index.html
+    .nodeCanvasObject((node, ctx, globalScale) => {
+        const label = node.id;
+        const fontSize = 12/globalScale;
+        ctx.font = `${fontSize}px Sans-Serif`;
+        const textWidth = ctx.measureText(label).width;
+        const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillStyle = '#3183ba';
+        ctx.fillText(label, node.x, node.y);
+
+        node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+    })
+    .nodePointerAreaPaint((node, color, ctx) => {
+        ctx.fillStyle = color;
+        const bckgDimensions = node.__bckgDimensions;
+        bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+    });
