ad59ae7d1d045dfe9e4b1edda1bac7f1627304b3
Anton Khorev <tony29@yandex.ru>
2023-10-27 17:25:21 +0300

    create node with tags

--- node.pl
+++ node.pl
@@ -8,22 +8,30 @@ use OsmData;
 
 my $cid;
 my ($lat, $lon);
+my @keys;
+my @values;
 my $correct_options = GetOptions(
     "changeset|cid=i" => \$cid,
     "lat=f" => \$lat,
     "lon=f" => \$lon,
+    "key=s" => \@keys,
+    "value=s" => \@values,
 );
 
 if (($ARGV[0] eq "create") && (scalar(@ARGV) == 1) && $correct_options)
 {
     die "lat is missing" unless defined($lat);
     die "lon is missing" unless defined($lon);
+    die "different number of keys/values" unless @keys == @values;
+
+    my %tags;
+    @tags{@keys} = @values;
 
     my $body;
     open my $fh, '>', \$body;
     OsmData::print_fh_xml_header($fh);
     OsmData::print_fh_element($fh, OsmData::NODE, undef, undef, [
-        $cid, undef, undef, undef, {}, $lat * OsmData::SCALE, $lon * OsmData::SCALE
+        $cid, undef, undef, undef, \%tags, $lat * OsmData::SCALE, $lon * OsmData::SCALE
     ]);
     OsmData::print_fh_xml_footer($fh);
     close $fh;
