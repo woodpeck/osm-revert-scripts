9385bdb8ee0f4c7127026323c7f8b5173f198b7d
Anton Khorev <tony29@yandex.ru>
2024-12-08 14:15:19 +0300

    add arbitrary put request tool

--- api.pl
+++ api.pl
@@ -12,9 +12,19 @@ if (($ARGV[0] eq "get") && (scalar(@ARGV) == 2))
     exit;
 }
 
+if (($ARGV[0] eq "put") && (scalar(@ARGV) == 2))
+{
+    my $path = $ARGV[1];
+    my $body = "";
+    while(<STDIN>) { $body .= $_; }
+    OsmApi::put($path, $body, 1);
+    exit;
+}
+
 print <<EOF;
 Usage:
-  $0 get <path>    make get request
+  $0 get <path>                 get request
+  $0 put <path> < input_file    put request
 
 <path> is relative to (server)/api/0.6/. For example, this command gets trace #23:
   $0 get gpx/23
