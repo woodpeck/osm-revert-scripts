6fc43f641ac9c0156f1609551aa4e21bd5af4825
Anton Khorev <tony29@yandex.ru>
2024-12-08 14:51:59 +0300

    add arbitrary post request tool

--- api.pl
+++ api.pl
@@ -19,6 +19,15 @@ if (($ARGV[0] eq "get") && (scalar(@ARGV) == 2))
     exit;
 }
 
+if (($ARGV[0] eq "post") && (scalar(@ARGV) == 2))
+{
+    my $path = $ARGV[1];
+    my $body = "";
+    while(<STDIN>) { $body .= $_; }
+    OsmApi::post($path, $body, 1);
+    exit;
+}
+
 if (($ARGV[0] eq "put") && (scalar(@ARGV) == 2))
 {
     my $path = $ARGV[1];
@@ -30,9 +39,10 @@ if (($ARGV[0] eq "put") && (scalar(@ARGV) == 2))
 
 print <<EOF;
 Usage:
-  $0 delete <path>              DELETE request
-  $0 get <path>                 GET request
-  $0 put <path> < input_file    PUT request
+  $0 delete <path>               DELETE request
+  $0 get <path>                  GET request
+  $0 post <path> < input_file    POST request
+  $0 put <path> < input_file     PUT request
 
 <path> is relative to (server)/api/0.6/. For example, this command gets trace #23:
   $0 get gpx/23
