972928c38b5cb30116dafaa9eb890c5a366efb62
Anton Khorev <tony29@yandex.ru>
2025-03-26 03:39:17 +0300

    add stdin argument for osm_api.pl

--- osm_api.pl
+++ osm_api.pl
@@ -14,47 +14,41 @@ if (($ARGV[0] eq "curl") && (scalar(@ARGV) >= 2))
     exec "curl", "--oauth2-bearer", $token, @ARGV, $url;
 }
 
-if (($ARGV[0] eq "delete") && (scalar(@ARGV) == 2))
+sub run_verb($&)
 {
-    my $path = $ARGV[1];
-    OsmApi::delete($path, undef, 1);
-    exit;
+    my $verb = shift;
+    my $sub = shift;
+    if (($ARGV[0] eq $verb) && (scalar(@ARGV) == 2 || (scalar(@ARGV) == 3 && $ARGV[2] eq "-")))
+    {
+        my $path = $ARGV[1];
+        my $body;
+        if ($ARGV[2] eq "-")
+        {
+          $body = "";
+          while(<STDIN>) { $body .= $_; }
+        }
+        &$sub($path, $body, 1);
+        exit;
+    }
 }
 
-if (($ARGV[0] eq "get") && (scalar(@ARGV) == 2))
-{
-    my $path = $ARGV[1];
-    OsmApi::get($path, undef, 1);
-    exit;
-}
-
-if (($ARGV[0] eq "post") && (scalar(@ARGV) == 2))
-{
-    my $path = $ARGV[1];
-    my $body = "";
-    while(<STDIN>) { $body .= $_; }
-    OsmApi::post($path, $body, 1);
-    exit;
-}
-
-if (($ARGV[0] eq "put") && (scalar(@ARGV) == 2))
-{
-    my $path = $ARGV[1];
-    my $body = "";
-    while(<STDIN>) { $body .= $_; }
-    OsmApi::put($path, $body, 1);
-    exit;
-}
+run_verb "delete", \&OsmApi::delete;
+run_verb "get", \&OsmApi::get;
+run_verb "post", \&OsmApi::post;
+run_verb "put", \&OsmApi::put;
 
 print <<EOF;
 Usage:
   $0 curl <curl options> <path>    run cURL with proper --oauth2-bearer and url
-  $0 delete <path>                 DELETE request
-  $0 get <path>                    GET request
-  $0 post <path> < input_file      POST request
-  $0 put <path> < input_file       PUT request
+  $0 delete <path> [-]             DELETE request
+  $0 get <path> [-]                GET request
+  $0 post <path> [-]               POST request
+  $0 put <path> [-]                PUT request
 
 <path> is relative to (server)/api/0.6/
+
+delete, get, post and put have an optional last argument "-" to read the request body from stdin,
+otherwise an empty body is sent
 
 Examples:
   get trace #23:
